986463d640a9139f1873fcf8bee8c4f394619ea8
erica007 <erica777erica@gmail.com>
2022-09-25 18:33:26 -0700

    add generate codes on random js file

---
+++ js/randomcodes.js
@@ -0,0 +1,22 @@
+/* ----- RANDOM CODES -----*/
+
+//Function to generate combination of characters
+function generateCode() {
+    var code = ' '; 
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+    for (i = 1; i <= 8; i++) {
+        var char = Math.random()* str.length;
+        code += str.charAt(char)
+    }
+    return code;
+}
+
+document.getElementById("codes").innerHTML = generateCode();
+
+function disableButton() {
+    document.getElementById("submit").disabled = true;
+}
+
+disableButton();
+
