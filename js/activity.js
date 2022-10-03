709b311707326b6d308b86fa326d30de7253aa04
erica007 <erica777erica@gmail.com>
2022-10-02 20:10:45 -0700

    W5D1 Homework:  make activity table interactive

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-
-
-
-    
-})
+    $('table tbody tr td').not('td:first-child').hover(function() {
+        DisplayText = $(this).text();
+        if(DisplayText !== 'Not Available') {
+            $(this).css({cursor: 'pointer'})
+        }
+    }).click(function() {
+        var DisplayText;
+        DisplayText = $(this).text();
+        if(DisplayText !== 'Not Available') {
+            $(this).toggleClass("activitiesTableSelected");
+        } else { 
+            $(this).toggleClass("activitiesTablenotSelected");
+            
+        };
+    });
+});
