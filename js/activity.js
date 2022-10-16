121d99bb9135a9ddb25bd07173e5b945edb89782
erica007 <erica777erica@gmail.com>
2022-10-16 15:58:00 -0700

    W7D1 Homework

--- js/activity.js
+++ js/activity.js
@@ -1,41 +1,22 @@
-$(document).ready(function() {
-    $('td').not('td:first-child').hover(function() {
-        var DisplayText;
-        DisplayText = $(this).text();
-        if(DisplayText !== 'Not Available') {
-            $(this).css({cursor: 'pointer'})
-        }
+$(document).ready(function(){
+    $("#activitiesTable > thead > tr > td").addClass("notAvailable");
+    $("#activitiesTable > tbody > tr > td:first-child").addClass("notAvailable");
+    $("#activitiesTable > tbody > tr > td:contains('Not Available')").addClass("notAvailable");
+    $("#activitiesTable").find("td").not(".notAvailable").addClass("available");
+    $(".available").hover(function(){
+        $(this).css('cursor', 'pointer');
+    })
+    
+    $(".available").click(function(){
+        var index = $(this).index()+1;
+        $(this).toggleClass("activitiesTableSelected");
+        if($(this).hasClass("activitiesTableSelected")) {
+
+            $("#result").append('<p>'+$(this).text()+' at ' + $("#activitiesTableHead td:nth-child("+index+")").text() + '</p>'); //add child element with contents of cell
+        } else { 
+            $("#result  p:contains("+$(this).text()+')').remove(); 
+
+        };
+        $("#modalButton").trigger("click");
     });
-
-    $("td").click(function() {
-        var DisplayText;
-        var thIndex = $(this).index();
-        var thText = $("table").find("th").eq(thIndex).text()
-        DisplayText = $(this).text();
-      
-        if(DisplayText != 'Not Available') {
-            $(this).toggleClass("selected");
-
-            if ($(this).hasClass("selected")) {
-                $("#displaySelected").css("visibility", "visible");
-                $("#displaySelected").css("margin-top", "2em");
-                $("#result").append("<p>" + DisplayText + " at " + thText + "</p>");             
-            } else {
-                $("#result p:contains(" + DisplayText + ")").remove();
-
-                if ($("#result").has("p").length == false) {
-                    $("#displaySelected").css("visibility", "hidden");
-                    $("#displaySelected").css("margin-top", "0");
-                }
-            }
-        }
-    });
-
 });
-
-//Hide displaySelected when load
-function hidedisplaySelected() {
-    document.getElementById("displaySelected").style.visibility = "hidden";
-};
-
-hidedisplaySelected();
--- js/digclock.js
+++ js/digclock.js
@@ -1,60 +1,77 @@
+// Create a time data function
+function currentTime() {
+    // Declare variables
+    var d = new Date(); //Get current date
+    var hr = d.getHours(); //Get current hours
+    var min = d.getMinutes(); //Get current minutes
+    var sec = d.getSeconds(); //Get current seconds
+    var ampm; //Declare empty variable to store AM or PM
 
-            // create a time data function
-            function currentTime() {
-                // Declare Variables
-                var d = new Date(); //get the current date
-                var hr = d.getHours(); //get current hours
-                var min = d.getMinutes(); //GET current minutes
-                var sec = d.getSeconds(); //get current seconds
-                var ampm; //declare empty variable to store AM or PM
-                var utchr = d.getUTCHours(); //get the GMT
-                var timeDiff; 
-                var adjTimeDiff; 
-                var timeZone;
+    //Add 0 to single digits for seconds
+    if(sec < 10) {
+        sec = "0" + sec;
+    };
+    //Add 0 to single digits for minutes
+    if(min < 10) {
+        min = "0" + min;
+    };
 
-                // add 0 to single digits for seconds
-                if (sec < 10) {
-                    sec = "0" + sec;
-                }
-                // add 0 to single digits for minutes
-                if (min < 10) {
-                    min = "0" + min;
-                }
-                //Determin AM or PM string
-                if (hr == 12) {
-                    ampm = "PM";
-                } else if ( hr > 12) {
-                    hr -= 12;
-                    ampm = "PM";
-                } else {
-                    ampm = "AM";
-                }
+    //Determine AM ot PM string
+    if(hr == 12) {
+        ampm = "PM"; //Set to PM
+    } else if(hr > 12) {
+        hr -= 12; //Deduct 12 from hours greater than 12 (military time)
+        ampm = "PM"; //Set to PM
+    } else {
+        ampm = "AM"; //Set to AM
+    };
 
-                if (utchr > 12) {
-                    utchr -= 12;
-                } 
+    var utchr = d.getUTCHours(); //Get current Greenwich Mean Time(GMT)
+    var timeDiff = utchr - hr; //To store time difference between GMT hour and Local hour
+    var adjTimeDiff; //To store time difference converted to positive number
+    if (timeDiff < 0) {
+        adjTimeDiff = timeDiff + 12;
+    } else if (timeDiff > 11) {
+        adjTimeDiff = timeDiff - 12;
+    } else {
+        adjTimeDiff = timeDiff;
+    };
+    var timeZone; //To store the 4 time zones
+    // if (adjTimeDiff == 4) {
+    //     timeZone = "ET";
+    // } else if (adjTimeDiff == 5) {
+    //     timeZone = "CT";
+    // } else if (adjTimeDiff == 6) {
+    //     timeZone = "MT";
+    // } else if (adjTimeDiff == 7) {
+    //     timeZone = "PT";
+    // }
 
-                timeDiff = utchr + hr;
-                adjTimeDiff = Math.abs(timeDiff);
+    // Switch case takes longer time.
+    switch(adjTimeDiff) {
+        case '4':
+            timeZone = "ET";
+            break;
+        case 5:
+            timeZone = "CT";
+            break;
+        case 6:
+            timeZone = "MT";
+            break;
+        case 7:
+            timeZone = "PT";
+            break;
+    }
 
-                if(adjTimeDiff ==4) {
-                    timeZone = 'ET';
-                } else if (adjTimeDiff ==5) {
-                    timeZone = 'CT';
-                } else if (adjTimeDiff ==6) {
-                    timeZone = 'MT';
-                } else if (adjTimeDiff ==7) {
-                    timeZone = 'PT';
-                } else {
-                    timeZone = '';
-                }
+    //Assemble time format to display
+    var time = hr +  ":"  + min + ":" +  sec + " " +  ampm+ " " + timeZone;
 
-                // Assemble time format to display
-                var time = hr + ":"+ min +":" + sec + " "+ ampm + " " +timeZone;
-                document.getElementById("clock").innerText = time;
-                setInterval(currentTime,1000);
-            }
-            
-            currentTime();
+    //Display current local time and time zone on HTML elements
+    document.getElementById("clock").innerText = time; //adding time
 
-          
+    //Run time data function every 1 second
+    setInterval(currentTime, 1000); //setting timer
+}
+
+// Initial run of time data function
+currentTime();
